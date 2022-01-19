a5151dab3699538c694a28b41de77a7795ae00a2
Bryan Stern <bstern@slack-corp.com>
2022-01-19 11:30:28 -0700

    Formatting

--- slack-lint/src/main/java/slack/lint/DeprecatedAnnotationDetector.kt
+++ slack-lint/src/main/java/slack/lint/DeprecatedAnnotationDetector.kt
@@ -41,55 +41,55 @@ private const val BRIEF_DESCRIPTION_SUFFIX = " is deprecated; consider using an 
  */
 class DeprecatedAnnotationDetector : AnnotatedClassOrMethodUsageDetector() {
 
-    override val annotationNames = listOf(DEPRECATED_ANNOTATION_NAME_KOTLIN, DEPRECATED_ANNOTATION_NAME_JAVA)
-    override val issue = ISSUE_DEPRECATED_CALL
+  override val annotationNames = listOf(DEPRECATED_ANNOTATION_NAME_KOTLIN, DEPRECATED_ANNOTATION_NAME_JAVA)
+  override val issue = ISSUE_DEPRECATED_CALL
 
-    // Only enable on CLI
-    override val isEnabled: Boolean
-        get() = !LintClient.isStudio
+  // Only enable on CLI
+  override val isEnabled: Boolean
+    get() = !LintClient.isStudio
 
-    override fun visitAnnotationUsage(
-            context: JavaContext,
-            usage: UElement,
-            type: AnnotationUsageType,
-            annotation: UAnnotation,
-            qualifiedName: String,
-            method: PsiMethod?,
-            referenced: PsiElement?,
-            annotations: List<UAnnotation>,
-            allMemberAnnotations: List<UAnnotation>,
-            allClassAnnotations: List<UAnnotation>,
-            allPackageAnnotations: List<UAnnotation>
-    ) {
-        if (isEnabled && applicableAnnotations().contains(qualifiedName)) {
-            val issueToReport = issue
-            val location = context.getLocation(usage)
-            val messagePrefix = referenced?.let(UastLintUtils.Companion::getQualifiedName)
-                    ?: BRIEF_DESCRIPTION_PREFIX_DEFAULT
-            report(
-                    context,
-                    issueToReport,
-                    usage,
-                    location,
-                    messagePrefix + BRIEF_DESCRIPTION_SUFFIX,
-                    null
-            )
-        }
+  override fun visitAnnotationUsage(
+    context: JavaContext,
+    usage: UElement,
+    type: AnnotationUsageType,
+    annotation: UAnnotation,
+    qualifiedName: String,
+    method: PsiMethod?,
+    referenced: PsiElement?,
+    annotations: List<UAnnotation>,
+    allMemberAnnotations: List<UAnnotation>,
+    allClassAnnotations: List<UAnnotation>,
+    allPackageAnnotations: List<UAnnotation>
+  ) {
+    if (isEnabled && applicableAnnotations().contains(qualifiedName)) {
+      val issueToReport = issue
+      val location = context.getLocation(usage)
+      val messagePrefix = referenced?.let(UastLintUtils.Companion::getQualifiedName)
+        ?: BRIEF_DESCRIPTION_PREFIX_DEFAULT
+      report(
+        context,
+        issueToReport,
+        usage,
+        location,
+        messagePrefix + BRIEF_DESCRIPTION_SUFFIX,
+        null
+      )
+    }
+  }
+
+  companion object {
+    private fun Implementation.toIssue(): Issue {
+      return Issue.create(
+        "DeprecatedCall",
+        BRIEF_DESCRIPTION_PREFIX_DEFAULT + BRIEF_DESCRIPTION_SUFFIX,
+        "Using deprecated classes is not advised; please consider using an alternative.",
+        Category.CORRECTNESS,
+        Priorities.NORMAL,
+        Severity.WARNING,
+        this
+      )
     }
 
-    companion object {
-        private fun Implementation.toIssue(): Issue {
-            return Issue.create(
-                    "DeprecatedCall",
-                    BRIEF_DESCRIPTION_PREFIX_DEFAULT + BRIEF_DESCRIPTION_SUFFIX,
-                    "Using deprecated classes is not advised; please consider using an alternative.",
-                    Category.CORRECTNESS,
-                    Priorities.NORMAL,
-                    Severity.WARNING,
-                    this
-            )
-        }
-
-        val ISSUE_DEPRECATED_CALL = sourceImplementation<DeprecatedAnnotationDetector>().toIssue()
-    }
+    val ISSUE_DEPRECATED_CALL = sourceImplementation<DeprecatedAnnotationDetector>().toIssue()
+  }
 }
