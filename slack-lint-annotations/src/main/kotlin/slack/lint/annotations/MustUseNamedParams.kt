e820e349ad330afd10c5985ffe08cfd329517521
Yifan Wu <ywu@slack-corp.com>
2022-03-21 15:12:31 -0700

    Fix named params annotation (#18)

--- slack-lint-annotations/src/main/kotlin/slack/lint/annotations/MustUseNamedParams.kt
+++ slack-lint-annotations/src/main/kotlin/slack/lint/annotations/MustUseNamedParams.kt
@@ -19,6 +19,6 @@ package slack.lint.annotations
  * Callers to this function must named all parameters. This is useful in cases where arguments may
  * change in order and you want to avoid source-breaking changes.
  */
-@Target(AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class MustUseNamedParams
