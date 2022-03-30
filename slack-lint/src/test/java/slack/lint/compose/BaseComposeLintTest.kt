8679a3d3a1ff74458750f95f4ff6cd17cfb0a8e4
Zac Sweers <pandanomic@gmail.com>
2022-03-30 01:55:30 -0400

    Add a BaseComposeLintTest

---
+++ slack-lint/src/test/java/slack/lint/compose/BaseComposeLintTest.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package slack.lint.compose
+
+import slack.lint.BaseSlackLintTest
+
+abstract class BaseComposeLintTest : BaseSlackLintTest() {
+
+  private val composableAnnotation = kotlin(
+    """
+      package androidx.compose.runtime
+
+      annotation class Composable
+    """
+  ).indented()
+
+  private val modifier = kotlin(
+    """
+      package androidx.compose.ui
+
+      interface Modifier
+    """
+  ).indented()
+
+  protected val commonFiles = arrayOf(composableAnnotation, modifier)
+}
