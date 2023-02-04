4fd6755786fc061b86a9f7b92f059e09362c0ee6
Zac Sweers <pandanomic@gmail.com>
2023-02-03 22:56:58 -0500

    Remove compose checks (#37)

--- slack-lint-checks/src/main/java/slack/lint/SlackIssueRegistry.kt
+++ slack-lint-checks/src/main/java/slack/lint/SlackIssueRegistry.kt
@@ -7,20 +7,6 @@ import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
 import com.google.auto.service.AutoService
-import slack.lint.compose.ComposeCompositionLocalNamingDetector
-import slack.lint.compose.ComposeCompositionLocalUsageDetector
-import slack.lint.compose.ComposeContentEmitterReturningValuesDetector
-import slack.lint.compose.ComposeModifierMissingDetector
-import slack.lint.compose.ComposeModifierReusedDetector
-import slack.lint.compose.ComposeModifierWithoutDefaultDetector
-import slack.lint.compose.ComposeMultipleContentEmittersDetector
-import slack.lint.compose.ComposeMutableParametersDetector
-import slack.lint.compose.ComposeNamingDetector
-import slack.lint.compose.ComposeParameterOrderDetector
-import slack.lint.compose.ComposePreviewNamingDetector
-import slack.lint.compose.ComposePreviewPublicDetector
-import slack.lint.compose.ComposeRememberMissingDetector
-import slack.lint.compose.ComposeUnstableCollectionsDetector
 import slack.lint.denylistedapis.DenyListedApiDetector
 import slack.lint.eithernet.DoNotExposeEitherNetInRepositoriesDetector
 import slack.lint.inclusive.InclusiveNamingChecker
@@ -77,21 +63,5 @@ class SlackIssueRegistry : IssueRegistry() {
       MissingResourceImportAliasDetector.ISSUE,
       WrongResourceImportAliasDetector.ISSUE,
       DenyListedApiDetector.ISSUE,
-      // <editor-fold desc="Compose Detectors">
-      *ComposeNamingDetector.ISSUES,
-      ComposeCompositionLocalNamingDetector.ISSUE,
-      ComposeCompositionLocalUsageDetector.ISSUE,
-      ComposeContentEmitterReturningValuesDetector.ISSUE,
-      ComposeModifierMissingDetector.ISSUE,
-      ComposeModifierReusedDetector.ISSUE,
-      ComposeModifierWithoutDefaultDetector.ISSUE,
-      ComposeMultipleContentEmittersDetector.ISSUE,
-      ComposeMutableParametersDetector.ISSUE,
-      ComposeParameterOrderDetector.ISSUE,
-      ComposePreviewNamingDetector.ISSUE,
-      ComposePreviewPublicDetector.ISSUE,
-      ComposeRememberMissingDetector.ISSUE,
-      ComposeUnstableCollectionsDetector.ISSUE,
-      // </editor-fold>
     )
 }
