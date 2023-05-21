99b17ca2f0f0fce857001c7e8acd38aa1dc234c5
Zac Sweers <pandanomic@gmail.com>
2023-05-21 14:46:04 -0400

    Modernize build infra

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
 import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -53,25 +54,28 @@ allprojects {
   }
 }
 
+val jvmTargetString = libs.versions.jvmTarget.get()
+val jvmTargetInt = jvmTargetString.toInt()
+
 subprojects {
   pluginManager.withPlugin("java") {
     configure<JavaPluginExtension> { toolchain { languageVersion.set(JavaLanguageVersion.of(19)) } }
 
-    tasks.withType<JavaCompile>().configureEach { options.release.set(11) }
+    tasks.withType<JavaCompile>().configureEach { options.release.set(jvmTargetInt) }
   }
 
   pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
     tasks.withType<KotlinCompile>().configureEach {
-      kotlinOptions {
-        jvmTarget = "11"
-        // TODO re-enable once lint uses Kotlin 1.5
-        //  allWarningsAsErrors = true
-        //  freeCompilerArgs = freeCompilerArgs + listOf("-progressive")
+      compilerOptions {
+        jvmTarget.set(JvmTarget.fromTarget(jvmTargetString))
+        // TODO re-enable if lint ever targets latest kotlin versions
+        //  allWarningsAsErrors.set(true)
+        //  freeCompilerArgs.add("-progressive")
       }
     }
   }
 
-  tasks.withType<Detekt>().configureEach { jvmTarget = "11" }
+  tasks.withType<Detekt>().configureEach { jvmTarget = jvmTargetString }
 
   pluginManager.withPlugin("com.vanniktech.maven.publish") {
     apply(plugin = "org.jetbrains.dokka")
