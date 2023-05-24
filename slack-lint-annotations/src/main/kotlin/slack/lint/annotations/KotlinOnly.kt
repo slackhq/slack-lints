a494b2929e7c0d40d6dc9e5321752d972ba39e34
Zac Sweers <pandanomic@gmail.com>
2023-05-24 00:14:53 -0400

    Update repo dependencies (#60)
          * Gradle 8.1.1
    
    * Modernize build infra
    
    * Add missing reason String to kotlin/java only annotations
    
    * Update deprecated API
    
    * Get javaonly test working again
    
    Java stub files got harder to use shrug
    
    * Update dependencies
    
    * Align min and current API
    
    * Add feedback and contact to vendor
    
    * Simplify CI
    
    * Fix value in JavaOnlyDetector
    
    * Clean up object detection APIs
    
    * Fix misaligned jvm targs
    
    * Spotless

--- slack-lint-annotations/src/main/kotlin/slack/lint/annotations/KotlinOnly.kt
+++ slack-lint-annotations/src/main/kotlin/slack/lint/annotations/KotlinOnly.kt
@@ -6,5 +6,7 @@ package slack.lint.annotations
  * An annotation that is used to denote methods that should not be called from any context other
  * than Kotlin code. This is important for cases in APIs that support both Kotlin and Java in
  * different ways.
+ *
+ * @property reason An optional reason why this API is intended for Java only.
  */
-annotation class KotlinOnly
+annotation class KotlinOnly(val reason: String = "")
