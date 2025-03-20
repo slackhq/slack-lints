554f86af09d43000fcb7c8eed2d3ba027bfc4e9b
ansehun <ansehoon1999@gmail.com>
2025-03-20 11:24:15 +0900

    Consider AlwaysNullReadOnlyVariableDetector's New Case & Add Additional Test Codes

--- slack-lint-checks/src/test/java/slack/lint/AlwaysNullReadOnlyVariableDetectorTest.kt
+++ slack-lint-checks/src/test/java/slack/lint/AlwaysNullReadOnlyVariableDetectorTest.kt
@@ -2,20 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 package slack.lint
 
-import com.android.tools.lint.checks.infrastructure.TestMode
 import org.junit.Test
 
 class AlwaysNullReadOnlyVariableDetectorTest : BaseSlackLintTest() {
-  override fun getDetector() = AlwaysNullReadOnlyVariableDetector()
+    override fun getDetector() = AlwaysNullReadOnlyVariableDetector()
 
-  override fun getIssues() = listOf(AlwaysNullReadOnlyVariableDetector.ISSUE)
+    override fun getIssues() = AlwaysNullReadOnlyVariableDetector.ISSUES.toList()
 
-  @Test
-  fun `initializing a read-only variable with null shows warnings`() {
-    lint()
-      .files(
-        kotlin(
-            """
+    @Test
+    fun `initializing a read-only variable with null shows warnings`() {
+        lint()
+            .files(
+                kotlin(
+                    """
             package foo
 
             class Test {
@@ -26,23 +25,165 @@ class AlwaysNullReadOnlyVariableDetectorTest : BaseSlackLintTest() {
                 }
             }
             """
-          )
-          .indented()
-      )
-      .skipTestModes(TestMode.JVM_OVERLOADS)
-      .allowDuplicates()
-      .run()
-      .expect(
-        """
-        src/foo/Test.kt:4: Warning: Avoid initializing read-only variable with null [AvoidNullInitializationForReadOnlyVariables]
+                )
+                    .indented()
+            ).allowMissingSdk()
+            .allowDuplicates()
+            .run()
+            .expect(
+                """
+        src/foo/Test.kt:4: Warning: Avoid initializing read-only variable with null in Kotlin [AvoidNullInitializationForReadOnlyVariables]
             val str: String? = null
                                ~~~~
-        src/foo/Test.kt:7: Warning: Avoid initializing read-only variable with null [AvoidNullInitializationForReadOnlyVariables]
+        src/foo/Test.kt:7: Warning: Avoid initializing read-only variable with null in Kotlin [AvoidNullInitializationForReadOnlyVariables]
                 val strInFunction: String? = null
                                              ~~~~
         0 errors, 2 warnings
         """
-          .trimIndent()
-      )
-  }
+                    .trimIndent()
+            )
+    }
+
+    @Test
+    fun `returning null in custom getter shows warnings`() {
+        lint()
+            .files(
+                kotlin(
+                    """
+                    package foo
+                    
+                    class Test {
+                        val str1: String? 
+                            get() = null
+
+                        val str2: String? 
+                            get() {
+                                return null
+                            }
+
+                        val str3: String? = null
+                            get() = field
+                    }
+            """
+                )
+                    .indented()
+            ).allowMissingSdk()
+            .allowDuplicates()
+            .run()
+            .expect(
+                """
+        src/foo/Test.kt:12: Warning: Avoid initializing read-only variable with null in Kotlin [AvoidNullInitializationForReadOnlyVariables]
+            val str3: String? = null
+                                ~~~~
+        src/foo/Test.kt:5: Warning: Avoid returning null in getter for read-only properties in Kotlin [AvoidReturningNullInGetter]
+                get() = null
+                        ~~~~
+        src/foo/Test.kt:9: Warning: Avoid returning null in getter for read-only properties in Kotlin [AvoidReturningNullInGetter]
+                    return null
+                           ~~~~
+        0 errors, 3 warnings
+        """
+                    .trimIndent()
+            )
+    }
+
+    @Test
+    fun `initializing a read-write variable with null doesn't show warnings`() {
+        lint()
+            .files(
+                kotlin(
+                    """
+                    package foo
+                    
+                    class Test {
+                        var str: String? = null
+    
+                        fun method() {
+                            var str: String = null
+                        }
+                    }
+                """
+                ).indented()
+            )
+            .allowMissingSdk()
+            .allowDuplicates()
+            .run()
+            .expectClean()
+    }
+
+    @Test
+    fun `initializing a read-only variable with not null doesn't show warnings`() {
+        lint()
+            .files(
+                kotlin(
+                    """
+                    package foo
+                    
+                    class Test {
+                        val str: String? = "str"
+    
+                        fun method() {
+                            val str: String? = "str"
+                        }
+                    }
+                """
+                ).indented()
+            )
+            .allowMissingSdk()
+            .allowDuplicates()
+            .run()
+            .expectClean()
+    }
+
+    @Test
+    fun `initializing a read-write variable with not null doesn't show warnings`() {
+        lint()
+            .files(
+                kotlin(
+                    """
+                    package foo
+                    
+                    class Test {
+                        var str: String? = "str"
+    
+                        fun method() {
+                            var str: String? = "str"
+                        }
+                    }
+                """
+                ).indented()
+            )
+            .allowMissingSdk()
+            .allowDuplicates()
+            .run()
+            .expectClean()
+    }
+
+    @Test
+    fun `returning not null in custom getter doesn't show warnings`() {
+        lint()
+            .files(
+                kotlin(
+                    """
+                    package foo
+                    
+                    class Test {
+                        val str1: String? 
+                            get() = "str"
+
+                        val str2: String? 
+                            get() {
+                                return "str"
+                            }
+                    }
+            """
+                )
+                    .indented()
+            ).allowMissingSdk()
+            .allowDuplicates()
+            .run()
+            .expectClean()
+    }
 }
+
+
