fda093af8798937f6882e9c5a2dacd509ea005a0
ansehun <ansehoon1999@gmail.com>
2025-03-30 11:19:22 +0900

    Change Detector Issue Position in Registry

--- slack-lint-checks/src/main/java/slack/lint/SlackIssueRegistry.kt
+++ slack-lint-checks/src/main/java/slack/lint/SlackIssueRegistry.kt
@@ -74,7 +74,7 @@ class SlackIssueRegistry : IssueRegistry() {
     add(NotNullOperatorDetector.ISSUE)
     add(DoNotCallViewToString.ISSUE)
     add(ItemDecorationViewBindingDetector.ISSUE)
-    addAll(AlwaysNullReadOnlyVariableDetector.ISSUES)
     add(NullableConcurrentHashMapDetector.ISSUE)
+    addAll(AlwaysNullReadOnlyVariableDetector.ISSUES)
   }
 }
